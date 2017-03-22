9187c2cfe4e299932a52bdcc72f3d0e9ae3e2050
kaixinguor <ling.wang.nj@gmail.com>
2017-03-22 08:13:28 +0100

    nvidia model

--- model.py
+++ model.py
@@ -9,6 +9,8 @@ from keras.layers import Convolution2D, Flatten
 from keras.layers import Dense, Activation, MaxPooling2D, Dropout
 import matplotlib.pyplot as plt
 import cv2
+import matplotlib.image as mpimg
+from PIL import Image
 
 
 # process image
@@ -58,7 +60,7 @@ def read_data():
     return car_images, steering_angles
 
 
-def build_model():
+def base_model():
     # build model with keras
     model = Sequential()
 
@@ -80,6 +82,36 @@ def build_model():
     model.add(Dense(128))
     model.add(Dense(64))
     model.add(Dense(1))
+    return model
+
+
+def nvidia_model():
+    # build model with keras
+    model = Sequential()
+
+    # cropping image
+    model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160, 320, 3))) # channels_last
+
+    # data normalization
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+
+    # five convolutional layers
+    model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+
+    # flatten
+    model.add(Flatten())
+
+    # three fully connected layers
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
     return model
 
 
@@ -105,28 +137,59 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
-if __name__ == '__main__':
-
+def data_exploration():
     driving_log_file = 'data/driving_log.csv'
-    img_path = 'data/IMG'
-
+    img_path = './data/IMG/'
     samples = []
     with open(driving_log_file) as csvfile:
         reader = csv.reader(csvfile)
 
         for line in reader:
             samples.append(line)
-    print(samples[0])
+        print(samples[0])
+        samples = samples[1:] # discard headline
 
-    samples = samples[1:]
 
+    sample = samples[0]
+    print(sample)
+
+    f,ax_arr = plt.subplots(2,3,figsize=(18,6))
+    imgs = []
+    for i in range(3):
+        name = img_path +  sample[i].split('/')[-1]
+        print(name)
+
+        # img = cv2.imread(name) #BGR
+        img = mpimg.imread(name) #RGB
+        #img = Image.open(name)  # RGB
+        print(img.shape)
+        ax_arr[0,i].imshow(img)
+        ax_arr[0,i].set_title(sample[i].split('/')[-1].split('_')[0],fontsize=20)
+
+        cropped_img = img[70:160 - 25, :, :]
+        ax_arr[1, i].imshow(cropped_img)
+        imgs.append(img)
+    plt.savefig('output_images/image_crop_70.png')
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    # read data and display
+    # data_exploration()
+
+    driving_log_file = 'data/driving_log.csv'
+    img_path = 'data/IMG'
+    samples = []
+    with open(driving_log_file) as csvfile:
+        reader = csv.reader(csvfile)
+
+        for line in reader:
+            samples.append(line)
+        print(samples[0])
+        samples = samples[1:] # discard headline
     print(len(samples))
 
-    train_samples, validation_samples = train_test_split(samples,test_size=0.2)
-
-    # compile and train the model using the generator function
-    train_generator = generator(train_samples, batch_size=32)
-    validation_generator = generator(validation_samples, batch_size=32)
 
     # ch, row, col = 3, 80, 320  # Trimmed image format
     #
@@ -136,7 +199,14 @@ if __name__ == '__main__':
     #
     # new_imgs, new_measurements = data_augment(car_images, steering_angles)
 
-    model = build_model()
+    train_samples, validation_samples = train_test_split(samples,test_size=0.2)
+
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
+
+
+    model = nvidia_model()
 
     # Compile and train the model
     model.compile(loss='mse', optimizer='adam')
@@ -144,6 +214,7 @@ if __name__ == '__main__':
                         validation_data = validation_generator, nb_val_samples = len(validation_samples),
                         nb_epoch = 3, verbose = 1)
     model.save('model.h5')
+
     ### print the keys contained in the history object
     print(history_object.history.keys())
 
@@ -154,9 +225,8 @@ if __name__ == '__main__':
     plt.ylabel('mean squared error loss')
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.savefig('output_images/training_fig.png')
     plt.show()
-
-    print("The validation accuracy is: %.3f.  It should be greater than 0.91" % history_object.history['val_acc'][-1])
 
 
 
