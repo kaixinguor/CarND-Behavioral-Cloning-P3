86050bdf5f7011bbefeae97faf4d7e798f5d0433
kaixinguor <ling.wang.nj@gmail.com>
2017-03-23 08:14:42 +0100

    add dropout and flip

--- model.py
+++ model.py
@@ -14,108 +14,93 @@ from PIL import Image
 
 
 # process image
-def process_image(image):
-    return image
+def preprocess_data(samples):
+
+    zeroItem = [sample for sample in samples if float(sample[3]) == 0]
+    nzeroItem = [sample for sample in samples if float(sample[3]) != 0]
+
+    print("number of sample with zero angle",len(zeroItem))
+    print("number of sample with non-zero angle", len(nzeroItem))
+
+    sklearn.utils.shuffle(zeroItem)
+    sample_num = np.floor(len(zeroItem)*0.2).astype(int)
+    print("sample from zero angle",sample_num)
+
+    xsamples = nzeroItem + zeroItem[0:sample_num]
+    print("{0:d} / {1:d} is kept".format(len(xsamples),len(samples)))
+    return xsamples
 
 
-def data_augment(imgs, measurements):
-    new_imgs = []
-    new_measurements = []
-    num_img = len(imgs)
-    for i in range(num_img):
-        img_flipped = np.fliplr(imgs[i])
-        measurement_flipped = -measurements[i]
-        new_imgs.append(img_flipped)
-        new_measurements.append(measurement_flipped)
-
-    return new_imgs, new_measurements
-
-
-def read_data():
+def get_sample_data(csv_file,img_path):
     # read data
-    csv_file = 'data/driving_log.csv'
-    path = 'data/IMG'
 
-    car_images = []
-    steering_angles = []
+    lineIdx = 0
+    samples = []
+    angles = []
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            steering_center = float(row[3])
+            if lineIdx == 0: # discard headline
+                lineIdx = 1
+                continue
 
-            # create adjusted steering measurements for the side camera images
-            correction = 0.2  # this is a parameter to tune
-            steering_left = steering_center + correction
-            steering_right = steering_center - correction
+            # attach image path
+            for i in range(3):
+                row[i] = img_path + row[i] .split('/')[-1]
 
-            # read in images from center, left and right cameras
-            # sdirectory = "..."  # fill in the path to your training IMG directory
-            img_center = process_image(np.asarray(Image.open(path + row[0])))
-            img_left = process_image(np.asarray(Image.open(path + row[1])))
-            img_right = process_image(np.asarray(Image.open(path + row[2])))
-
-            # add images and angles to data set
-            car_images.extend(img_center, img_left, img_right)
-            steering_angles.extend(steering_center, steering_left, steering_right)
-    return car_images, steering_angles
-
-
-def base_model():
-    # build model with keras
-    model = Sequential()
-
-    # cropping image
-    model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160, 320, 3))) # channels_last
-
-    # data normalization
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-
-    # convolutional layer
-    model.add(Convolution2D(8, 5, 5, subsample=(2,2), activation='relu'))
-    model.add(Convolution2D(16, 5, 5, subsample=(2,2), activation='relu'))
-    model.add(Convolution2D(32, 5, 5, subsample=(2,2), activation='relu'))
-
-
-    model.add(Flatten())
-
-    # fully connected
-    model.add(Dense(128))
-    model.add(Dense(64))
-    model.add(Dense(1))
-    return model
+            samples.append(row)
+            angles.append(float(row[3]))
+    return np.array(samples)
 
 
 def nvidia_model():
-    # build model with keras
+    '''
+    build model with keras. Reference paper:
+    End to End Learning for Self Driving Cars
+    Mariusz Bojarski, Davide Del Testa, Daniel Dworakowski, Bernhard Firner, Beat Flepp, Prasoon Goyal, Lawrence D.Jackel, Mathew Monfort, Urs Muller, Jiakai Zhang, Xin Zhang, Jake Zhao, Karol Zieba
+    By NIVIDIA 2016.
+    '''
+
     model = Sequential()
 
     # cropping image
-    model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160, 320, 3))) # channels_last
+    model.add(Cropping2D(cropping=((65,25),(0,0)),input_shape=(160, 320, 3))) # channels_last
 
     # data normalization
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
     # five convolutional layers
     model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
 
     model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Dropout(0.5))
 
     # flatten
     model.add(Flatten())
 
     # three fully connected layers
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
+    model.add(Dense(100,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(50,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10,activation='relu'))
+    model.add(Dropout(0.5))
+
+    # output layer
     model.add(Dense(1))
 
     return model
 
 
-def generator(samples, batch_size=32): # use only centr image
+def generator(samples, batch_size=32): # use center, left, right images
+
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         np.random.shuffle(samples)
@@ -125,56 +110,27 @@ def generator(samples, batch_size=32): # use only centr image
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './data/IMG/'+batch_sample[0].split('/')[-1]
-                #center_image = cv2.imread(name)
-                center_image = np.asarray(Image.open(name))
-                #center_image = cv2.cvtColor(center_image, cv2.COLOR_RGB2YUV)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
 
-            # trim image to only see section with road
+                for i in range(3):  # use center / left / right images
+                    name = batch_sample[i]
+                    image = np.asarray(Image.open(name))
+                    #center_image = cv2.cvtColor(center_image,cv2.COLOR_RGB2YUV) # this might be needed for generalization
+                    angle = float(batch_sample[3])
+                    if i==1:
+                        angle += 0.2
+                    elif i==2:
+                        angle -= 0.2
+                    images.append(image)
+                    angles.append(angle)
+
+                    # flip image
+                    images.append(np.fliplr(image))
+                    angles.append(-angle)
+
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-
-def generator2(samples, batch_size=32): # use center, left, right images
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        np.random.shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name = './data/IMG/'+batch_sample[0].split('/')[-1]
-                center_image = np.asarray(Image.open(name))
-                #center_image = cv2.cvtColor(center_image,cv2.COLOR_RGB2YUV)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-
-                # left image
-                name = './data/IMG/' + batch_sample[1].split('/')[-1]
-                left_image = np.asarray(Image.open(name))
-                #left_image = cv2.cvtColor(left_image,cv2.COLOR_RGB2YUV)
-                left_angle = float(batch_sample[3]) + 0.2
-                images.append(left_image)
-                angles.append(left_angle)
-
-                name = './data/IMG/' + batch_sample[2].split('/')[-1]
-                right_image = np.asarray(Image.open(name))
-                #right_image = cv2.cvtColor(right_image, cv2.COLOR_RGB2YUV)
-                right_angle = float(batch_sample[3]) - 0.2
-                images.append(right_image)
-                angles.append(right_angle)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
 
 def data_exploration():
     driving_log_file = 'data/driving_log.csv'
@@ -188,16 +144,21 @@ def data_exploration():
         print(samples[0])
         samples = samples[1:] # discard headline
 
-
-    sample = samples[0]
+    sample = samples[2200]
     print(sample)
 
     f,ax_arr = plt.subplots(2,3,figsize=(18,6))
     imgs = []
+    angs = []
     for i in range(3):
         name = img_path +  sample[i].split('/')[-1]
         print(name)
-
+        angle = float(sample[3])
+        angs.append(angle)
+        if i==1:
+            angle += 0.2
+        elif i==2:
+            angle -=0.2
         # img = cv2.imread(name) #BGR
         #img = mpimg.imread(name) #RGB
         img = np.asarray(Image.open(name))  # RGB
@@ -205,32 +166,30 @@ def data_exploration():
         ax_arr[0,i].imshow(img)
         ax_arr[0,i].set_title(sample[i].split('/')[-1].split('_')[0],fontsize=20)
 
-        cropped_img = img[70:160 - 25, :, :]
+        cropped_img = img[65:160 - 25, :, :]
         ax_arr[1, i].imshow(cropped_img)
+        ax_arr[1, i].set_xlabel('steering {0:.4f}'.format(angle), fontsize=20)
+
         imgs.append(img)
-    plt.savefig('output_images/image_crop_70.png')
+    plt.savefig('output_images/image_crop_65.png')
     plt.show()
 
-    # change color space
-    f, ax_arr = plt.subplots(2, 3, figsize=(18, 6))
-    imgs = []
-    for i in range(3):
-        name = img_path + sample[i].split('/')[-1]
-        print(name)
 
-        # img = cv2.imread(name) #BGR
-        # img = mpimg.imread(name) #RGB
-        img = np.asarray(Image.open(name) ) # RGB
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        print(img.shape)
-        ax_arr[0, i].imshow(img)
-        ax_arr[0, i].set_title(sample[i].split('/')[-1].split('_')[0], fontsize=20)
+    img = imgs[0]
+    angle = angs[0]
+    f, ax_arr = plt.subplots(1, 2, figsize=(18, 6))
+    ax_arr[0].imshow(img)
+    ax_arr[0].set_title('original image',fontsize=20)
+    ax_arr[0].set_xlabel('steering {0:.4f}'.format(angle), fontsize=20)
 
-        cropped_img = img[70:160 - 25, :, :]
-        ax_arr[1, i].imshow(cropped_img)
-        imgs.append(img)
-    plt.savefig('output_images/image_crop_70.png')
+    ax_arr[1].imshow(np.fliplr(img))
+    ax_arr[1].set_title('flipped image', fontsize=20)
+    ax_arr[0].set_xlabel('steering {0:.4f}'.format(-angle), fontsize=20)
+
+    plt.savefig('output_images/image_flip.png')
     plt.show()
+
+
 
 if __name__ == '__main__':
 
@@ -238,40 +197,37 @@ if __name__ == '__main__':
     # data_exploration()
     # exit(0)
 
-    driving_log_file = 'data/driving_log.csv'
-    img_path = 'data/IMG'
-    samples = []
-    with open(driving_log_file) as csvfile:
-        reader = csv.reader(csvfile)
+    # read data
+    csv_file = './data/driving_log.csv'
+    img_path = './data/IMG/'
+    samples = get_sample_data(csv_file,img_path)
 
-        for line in reader:
-            samples.append(line)
-        print(samples[0])
-        samples = samples[1:] # discard headline
-    print(len(samples))
+    samples = preprocess_data(samples)
+    angles = [float(sample[3]) for sample in samples]
+    # plt.hist(angles)
+    # plt.savefig('output_images/angle_hist_sample.png')
+    # plt.show()
+    # exit(0)
 
+    # samples = samples[np.arange(0,num_sample,2)]
+    # angles = angles[np.arange(0,num_sample,2)]
 
-    # ch, row, col = 3, 80, 320  # Trimmed image format
-    #
-    #
-    #
-    # car_images, steering_angles = read_data()
-    #
-    # new_imgs, new_measurements = data_augment(car_images, steering_angles)
-
-    train_samples, validation_samples = train_test_split(samples,test_size=0.2)
+    # randomly split into training - validation set
+    rand_state = np.random.randint(0, 100)
+    train_samples, validation_samples = train_test_split(samples,test_size=0.2,random_state=rand_state)
 
     # compile and train the model using the generator function
-    train_generator = generator2(train_samples, batch_size=32)
+    train_generator = generator(train_samples, batch_size=32)
     validation_generator = generator(validation_samples, batch_size=32)
 
+    # CNN architecture
     model = nvidia_model()
 
     # Compile and train the model
     model.compile(loss='mse', optimizer='adam')
-    history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*3,
-                        validation_data = validation_generator, nb_val_samples = len(validation_samples),
-                        nb_epoch = 3, verbose = 1)
+    history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*6,
+                        validation_data = validation_generator, nb_val_samples = len(validation_samples)*6,
+                        nb_epoch = 20, verbose = 1)
     model.save('model.h5')
 
     ### print the keys contained in the history object
--- writeup_report.md
+++ writeup_report.md
@@ -1,26 +1,17 @@
-#**Behavioral Cloning** 
+## Behavioral Cloning Project
 
-##Writeup Template
-
-###You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
-
----
-
-**Behavrioal Cloning Project**
-
-The goals / steps of this project are the following:
+### The goals / steps of this project are the following:
 * Use the simulator to collect data of good driving behavior
 * Build, a convolution neural network in Keras that predicts steering angles from images
 * Train and validate the model with a training and validation set
 * Test that the model successfully drives around track one without leaving the road
 * Summarize the results with a written report
 
-
 [//]: # (Image References)
-
-[image1]: ./output_images/error_nvidia1.png "Model Visualization"
-[image2]: ./examples/placeholder.png "Grayscaling"
-[image3]: ./examples/placeholder_small.png "Recovery Image"
+[image0]: ./output_images/model.png "Model in the paper"
+[image1]: ./output_images/image_crop_65.png "Cropping"
+[image2]: ./output_images/image_flip.png "Flip Image"
+[image3]: ./output_images/error.png "Error"
 [image4]: ./examples/placeholder_small.png "Recovery Image"
 [image5]: ./examples/placeholder_small.png "Recovery Image"
 [image6]: ./examples/placeholder_small.png "Normal Image"
@@ -50,86 +41,110 @@ python drive.py model.h5
 
 The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
 
-###Model Architecture and Training Strategy
-
-####1. An appropriate model arcthiecture has been employed
-
-I start with NVIDIA model.
-
-The training / validation error is a bit strange. 
-
-![alt text][image1]
-
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
-
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
-
-####2. Attempts to reduce overfitting in the model
-
-The model contains dropout layers in order to reduce overfitting (model.py lines 21). 
-
-The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
-
-####3. Model parameter tuning
-
-The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 25).
-
-####4. Appropriate training data
-
-Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road ... 
-
-For details about how I created the training data, see the next section. 
+---
 
 ###Model Architecture and Training Strategy
 
-####1. Solution Design Approach
 
-The overall strategy for deriving a model architecture was to ...
+I start with this paper:
 
-My first step was to use a convolution neural network model similar to the ... I thought this model might be appropriate because ...
+End to End Learning for Self-Driving Cars. Mariusz Bojarski, Davide Del Testa, Daniel Dworakowski, Bernhard Firner, Beat Flepp, Prasoon Goyal, Lawrence D. Jackel, Mathew Monfort, Urs Muller, Jiakai Zhang, Xin Zhang, Jake Zhao, Karol Zieba, By NIVIDIA 2016.
 
-In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
+I think this model would be a good start because it is used to train a network to drive a real self-driving car. They leveraged ~ 100 hours real data and showed that this artichitecture allows to train a good model which gives ~98% autonomy.
 
-To combat the overfitting, I modified the model so that ...
+The architecture introduced in the paper is like:
+![alt text][image0]
 
-Then I ... 
+The paper use 66x220x3 images in YUV color space.
 
-The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
 
-At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
+####1. Model arcthiecture
 
-####2. Final Model Architecture
+My network is very similar with NIVIDIA network. It is composed of three convolutional layers and three fully connected layers.
 
-The final model architecture (model.py lines 18-24) consisted of a convolution neural network with the following layers and layer sizes ...
+The implementation is in the function `nvidia_model` in lines 52 through 87 in the file `./model.py`.
+The final model architecture consists of a convolution neural network with the following layers and layer sizes:
 
-Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
+* Input RGB image (3@160x320)
+* Cropping layer (3@70x320)
+* Normalization layer (3@70x320)
+* Conv layer (24 filters with size 5x5, stride 2x2, padding 'valid') + Relu activation. Output is 24@32x158
+* Conv layer (36 filters with size 5x5, stride 2x2, padding 'valid') + Relu activation. Output is 36@14x77
+* Conv layer (48 filters with size 5x5, stride 2x2, padding 'valid') + Relu activation. Output is 48@5x37
+* Conv layer (64 filters with size 3x3, stride 1x1, padding 'valid') + Relu activation. Output is 64@3x35
+* Conv layer (64 filters with size 3x3, stride 1x1, padding 'valid') + Relu activation. Output is 64@1x33
+* Flatten -> 2112 nodes
+* Fully connected + Relu activation. Output 100 nodes.
+* Fully connected + Relu activation. Output 50 nodes.
+* Fully connected + Relu activation. Output 10 nodes.
+* Output -> 1 node
+
+The model take RGB images of original size (with lowest resolution 160x320x3). Cropping and normalization is added directly to the beginning layers to the network so that the computation could leverage the GPU and the same operation will be done on test image automatically.
+
+The network first crops the image to remove upper part which contain major far background like sky, trees which would not be very helpful for driving decision. The lowest part is also removed since it contains car hook.
+
+ The following figure shows a comparison between original images (upper row) and cropped images (bottom):
 
 ![alt text][image1]
 
-####3. Creation of the Training Set & Training Process
+The second layer does the normalization on the cropped images. Pixel values are normalized to `[-0.5, 0.5]`
+
+The following layers look like the architecture introduced in the paper, 
+Dropout layers are added to prevent overfitting.
+
+
+####2. Data Collection
+
+I used sample images from center camera, left camera and right camera with error correction to do the training. 
+
+I tried to collect own data to train, the result is similar but the driving routine is not very smooth. So then I only use sample images to train the model.
+
+
+I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting.
+
+Then I flip every image to augmente dataset.
+![alt text][image2]
 
 To capture good driving behavior, I first recorded two laps on track one using center lane driving. Here is an example image of center lane driving:
 
-![alt text][image2]
-
 I then recorded the vehicle recovering from the left side and right sides of the road back to center so that the vehicle would learn to .... These images show what a recovery looks like starting from ... :
-
-![alt text][image3]
-![alt text][image4]
-![alt text][image5]
 
 Then I repeated this process on track two in order to get more data points.
 
 To augment the data sat, I also flipped images and angles thinking that this would ... For example, here is an image that has then been flipped:
 
-![alt text][image6]
-![alt text][image7]
-
-Etc ....
-
-After the collection process, I had X number of data points. I then preprocessed this data by ...
 
 
-I finally randomly shuffled the data set and put Y% of the data into a validation set. 
+####3. Creation of the Training Set & Training Process
 
-I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was Z as evidenced by ... I used an adam optimizer so that manually training the learning rate wasn't necessary.
+The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16).
+
+I random shuffled the data set and put 20%  of the data into a validation set, in order to get an idea about whether the model is over or under fitting. . For splitting the data I use `sklearn.model_selection.train_test_split` in line 177 in `./model.py`.
+
+The model uses an adam optimizer, so that the learning rate is adapted automatically during training. (`./model.py` line 211). The training process optimizes mean square error between network output and ground truth steering angle.
+
+I sampled evenly 50% of the data since the neighboring images look very similar and less images will reduce training time.
+
+Finally 19284 images are used in training.
+
+I used 20 epochs.
+
+![alt text][image3]
+
+
+#### 4. Test
+
+The final step was to run the simulator to see how well the car was driving around track one. 
+
+It was actually suprising that using only the sample images to training three epochs already enables the car to drive autonomously around the track without leaving the road.
+
+There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
+
+Here's a [link to my video result](./result_track1.mp4)
+
+### Discussion
+
+*  If the driving condition contains a lot of change in lighting condition, then maybe transform RGB image into other color space will increase robustness (In NVIDIA's paper, they actually use YUV space). But here in the simulator, there is not much change in the illumination. So I use directly RGB color space.
+
+
+ 
